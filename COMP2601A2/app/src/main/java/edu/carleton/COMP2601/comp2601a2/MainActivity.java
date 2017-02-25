fe00ea583501115e63afdba4bbd5d1dadd19a7a2
Alexei Tipenko <atipenko2@gmail.com>
2017-02-25 11:58:25 -0500

    promptUser()

--- COMP2601A2/app/src/main/java/edu/carleton/COMP2601/comp2601a2/MainActivity.java
+++ COMP2601A2/app/src/main/java/edu/carleton/COMP2601/comp2601a2/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.carleton.COMP2601.comp2601a2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import org.json.JSONObject;
@@ -25,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
     static ObjectOutputStream oos;
 
     MessageReactor messageReactor;
+    private String nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        promptName();
+        
         array = new ArrayList<String>();
         instance = this;
 
@@ -65,6 +71,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static MainActivity getInstance() { return instance; }
+
+
+    public void promptName() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                nameText = input.getText().toString();
+            }
+        });
+        builder.show();
+    }
 
 
     public void updateListView(Message mes) {
